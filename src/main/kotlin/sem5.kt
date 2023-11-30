1947ff70eafe00c3e078d66e389f781b92ef5100
Alyona Drozdova <amdrozdova_1@edu.hse.ru>
2023-11-30 19:10:15 +0300

    sem5

---
+++ src/main/kotlin/sem5.kt
@@ -0,0 +1,50 @@
+open class Pet(val name: String) {
+    override fun toString(): String {
+        return ""
+    }
+}
+
+class Cat(name: String): Pet(name) {
+    override fun toString(): String {
+        return "MEOW"
+    }
+}
+
+
+class Dog(name: String): Pet(name) {
+    override fun toString(): String {
+        return "BARK"
+    }
+}
+
+class PetContainer<T: Pet> {
+    private val petList = mutableListOf<T>()
+
+    fun putIntoContainer(pet: T) {
+        petList.add(pet)
+    }
+
+    fun hearVoice(): T {
+        val randomPet = petList.random()
+        println("A pet's voice came through the container: $randomPet. Can you guess who this is?")
+        return randomPet
+    }
+}
+fun main() {
+    val container = PetContainer<Pet>()
+    container.putIntoContainer(Cat("Krosha"))
+    container.putIntoContainer(Dog("Lexus"))
+    container.putIntoContainer(Cat("Wice"))
+    container.putIntoContainer(Dog("Berta"))
+    container.putIntoContainer(Cat("Yasha"))
+
+    val randomPet = container.hearVoice();
+    println("Enter 0 if a dog and 1 if a cat: ")
+    val guess = readlnOrNull()?.toIntOrNull()
+
+    if ((guess == 0) and (randomPet is Dog) or (guess == 1) and (randomPet is Cat)) {
+        println("You won! Congratulations!")
+    } else {
+        println("You've lost :(")
+    }
+}
